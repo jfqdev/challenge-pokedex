0f808cc0f518eded1dcff7eb51e852753e2714d8
JFQ_DEV <jfqdev@gmail.com>
2022-04-28 17:29:49 -0300

    Change - Limit Pokemons on default request

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 const baseURL = ()=>{
-    return 'https://pokeapi.co/api/v2/pokemon/'
+    return 'https://pokeapi.co/api/v2/pokemon?limit=8&offset=0'
 }
 
 Vue.prototype.$axios = axios
